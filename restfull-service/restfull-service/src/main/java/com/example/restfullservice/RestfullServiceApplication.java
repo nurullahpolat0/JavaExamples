22c66065d63d28e661d7c0ae6347058a0e4bbaf7
Nurullah Polat <nurullahpolat0@gmail.com>
2023-03-18 00:03:40 +0300

    first commit all examples projects 18.03.2023

---
+++ restfull-service/restfull-service/src/main/java/com/example/restfullservice/RestfullServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfullservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfullServiceApplication.class, args);
+	}
+
+}
